206aa74155b97fe421232c540215fbec0ca6e6ec
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-01-17 21:25:35 +0200

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './layout/header/header.component';
+
+import { MaterialModule } from './shared/modules/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
