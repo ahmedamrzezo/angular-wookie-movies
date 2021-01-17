9e7cf455e81faea663dd1a06f80974daf643ea58
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-01-17 21:37:58 +0200

    movies module and lazy load it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'core/movies', loadChildren: () => import('./core/movies/movies.module').then(m => m.MoviesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
