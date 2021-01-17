51d8ea664154b97252d53af68692996d00b5b9d5
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-01-18 00:22:38 +0200

    movie details view

--- src/app/core/movies/components/movie-details/movie-details.component.ts
+++ src/app/core/movies/components/movie-details/movie-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.scss']
 })
 export class MovieDetailsComponent implements OnInit {
+  selectedMovie: any;
 
-  constructor() { }
+  year;
+
+  constructor(private actRoute: ActivatedRoute, private moviesServices: MovieService) { }
 
   ngOnInit(): void {
+    this.actRoute.params.subscribe(param => {
+      const slug = param.slug;
+
+      if (!this.moviesServices.movies) {
+        this.moviesServices.getMovies().subscribe(movies => {
+          this.selectedMovie = movies.find(movie => movie.slug === slug);
+
+          const slugSplitted = this.selectedMovie.slug.split('-');
+          this.year = slugSplitted[slugSplitted.length - 1];
+        });
+      } else {
+        this.selectedMovie = this.moviesServices.movies.find(movie => movie.slug === slug);
+
+        const slugSplitted = this.selectedMovie.slug.split('-');
+        this.year = slugSplitted[slugSplitted.length - 1];
+      }
+    });
   }
 
 }
